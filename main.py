2a5a46b65417df4f75ac40e0df2bbc25fb7e5434
shubhamk <170974454+rustyfae@users.noreply.github.com>
2026-05-22 22:45:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ TOKEN = os.getenv("TOKEN")
 intents = discord.Intents.all()
 
 bot = commands.Bot(
-    command_prefix='!pomo ',
+    command_prefix='!',
     intents=intents,
     help_command=None
 )
